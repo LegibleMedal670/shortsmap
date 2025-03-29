ad37f2a45d6826c695d188f57a19008f91d7d4a0
LegibleMedal670 <legiblemedal670@gmail.com>
2025-03-29 15:14:18 +0900

    :sparkles: feat : 영상 필터 Average Price 경우에 따라 적절한 double값으로 변경해주는 기능 추가 ( 영문으로 변경 필요 )

--- lib/Shorts/provider/FilterProvider.dart
+++ lib/Shorts/provider/FilterProvider.dart
@@ -10,24 +10,66 @@ class FilterProvider extends ChangeNotifier {
   double filterDistanceInKm = 1.5;
   bool filterByDistance = false;
 
-  void setBasicVideoCategory(String? region, String? category, double? price) {
+  void setBasicVideoCategory(String? region, String? category, String? price) {
+
+    double? priceToDouble;
+
+    if (price != null) {
+      switch (price) {
+        case '1만원 미만' :
+          priceToDouble = 10;
+          break;
+        case '1~2만원' :
+          priceToDouble = 20;
+          break;
+        case '2만원 이상' :
+          priceToDouble = 50;
+          break;
+        default :
+          priceToDouble = 50;
+          break;
+
+      }
+    }
 
     filterRegion = region;
     filterCategory = category;
-    filterPrice = price;
+    filterPrice = priceToDouble;
+
+    print('$filterRegion + $filterCategory + $filterPrice');
 
     notifyListeners();
   }
 
-  Future<void> setNearVideoCategory(String? category, double? price,) async {
+  Future<void> setNearVideoCategory(String? category, String? price,) async {
 
     final position = await Geolocator.getCurrentPosition();
+
+    double? priceToDouble;
+
+    if (price != null) {
+      switch (price) {
+        case '1만원 미만' :
+          priceToDouble = 10;
+          break;
+        case '1~2만원' :
+          priceToDouble = 20;
+          break;
+        case '2만원 이상' :
+          priceToDouble = 50;
+          break;
+        default :
+          priceToDouble = 50;
+          break;
+
+      }
+    }
 
     filterLat = position.latitude;
     filterLon = position.longitude;
     filterRegion = null;
     filterCategory = category;
-    filterPrice = price;
+    filterPrice = priceToDouble;
     filterByDistance = true;
 
     notifyListeners();
