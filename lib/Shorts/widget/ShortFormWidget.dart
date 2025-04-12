f2163f397360c80868f01bb1f9e91c3e8dc6bb77
LegibleMedal670 <legiblemedal670@gmail.com>
2025-04-12 19:26:26 +0900

    :sparkles: feat : 본 영상 저장 캐시 길이 200 이상 넘어가면 초기화

--- lib/Shorts/widget/ShortFormWidget.dart
+++ lib/Shorts/widget/ShortFormWidget.dart
@@ -247,6 +247,11 @@ class _ShortFormWidgetState extends State<ShortFormWidget> {
       // Set을 이용해 중복 제거
       seenVideoIds = seenVideoIds.toSet().toList();
 
+      // 캐시에 200개 이상 쌓이면 초기화
+      if(seenVideoIds.length > 200) {
+        seenVideoIds = [];
+      }
+
       // 업데이트된 리스트 저장
       await preferences.setStringList('seenVideoIds', seenVideoIds);
 
