2e3abe3da3220617cc56fa218b031956156661bc
LegibleMedal670 <legiblemedal670@gmail.com>
2025-05-11 21:32:03 +0900

    :wrench: chore : 버전 업데이트

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 1
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.0.2"
     }
 
     signingConfigs {
