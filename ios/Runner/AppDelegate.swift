504452d21e511ef0d55d1c918c6e7b5824075390
LegibleMedal670 <legiblemedal670@gmail.com>
2025-05-11 20:54:25 +0900

    :sparkles: feat : 페이스북 SDK 넣음 ( 잘된지모르겠음; )

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import GoogleMaps
+import FBSDKCoreKit   // ← Facebook SDK import
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,12 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // 1. Facebook SDK 초기화
+    ApplicationDelegate.shared.application(
+      application,
+      didFinishLaunchingWithOptions: launchOptions
+    )
+
     GMSServices.provideAPIKey("AIzaSyC0fC5Xjg33ZeaBChPXIK-ijjblzI4SnB4")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
